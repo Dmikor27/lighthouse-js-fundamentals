f37babad4acc9ddc3c176aa6b007e609dad2ebee
Dmitriy <dmikor27@gmail.com>
2023-05-16 21:20:20 -0400

    Loopy Lighthouse 2

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -11,3 +11,21 @@ while (number <= 200) {
   }
   number = number + 1;
 }
+
+console.log(number);
+
+const loopyLighthouse = function (range, multiples, words) {
+  for (let i = range[0]; i <= range[1]; i++) {
+    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    } else if (i % multiples[0] === 0) {
+      console.log(words[0]);
+    } else if (i % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(i);
+    }
+  };
+};
+
+console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]));
